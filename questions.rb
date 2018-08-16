5558f37b526ba401aaba46590ded80ee43fce591
08bravery <08bravery@gmail.com>
2018-08-16 17:09:55 -0600

    made changes, final draft

--- questions.rb
+++ questions.rb
@@ -13,16 +13,36 @@
 
 # ==========================================================================================
 
-puts "Ask the Magic Eight Ball a question and type 1 you must, hmmm."
-puts "If done you are, type QUIT you must."
+require 'pry'
 
-puts "You must unlearn what you have learned."
-puts "Do pigs fly?"
-puts "When the fat lady sings."
-puts "Yes. Si. Oui!"
-puts "No. Non. Nine!"
-puts "Patience you must have, my young Padawan."
-puts "Powerful you have become, the dark side I sense in you."
-puts "Outlook not so good."
-puts "Fear is the path to the Dark Side."
-puts "Many of the truths that we cling to depend on what we do."
+def greeting
+  puts "Ask the Magic Eight Ball a question you must, hmmm.  Or if done you are, type QUIT you must."
+  
+  user = gets.strip.downcase
+
+  case user
+   when 'quit'
+    puts "May the Force be with you!"
+    exit
+   when 'am i one with the force?'
+    puts "The Force is strong your family!"
+   else 
+      eightball_answers
+  
+  end
+    
+end
+
+def eightball_answers
+  answers = ["You must unlearn what you have learned.--- Type 'quit' to EXIT",
+  "Yes. Si. Oui!--- Type 'quit' to EXIT", "No. Non. Nine!--- Type 'quit' to EXIT",
+  "Patience you must have, my young Padawan.--- Type 'quit' to EXIT",
+  "Powerful you have become, the dark side I sense in you.--- Type 'quit' to EXIT", 
+  "Outlook not so good.---Type 'quit' to EXIT", "Fear is the path to the Dark Side.---Type 'quit' to EXIT",
+  "Many of the truths that we cling to depend on what we do.---Type 'quit' to EXIT"]
+  puts answers.sample
+  greeting
+  
+end
+
+greeting
