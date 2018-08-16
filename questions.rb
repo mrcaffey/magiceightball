ed446d1256d90c254bf7af611eb9c0b403cc13d8
08bravery <08bravery@gmail.com>
2018-08-16 15:17:03 -0600

    eight ball answers

---
+++ questions.rb
@@ -0,0 +1,28 @@
+# MAGIC EIGHT BALL OBJECTIVES
+# - User inputs question
+# - Computer outputs random answer
+# - User inputs "QUIT"
+# - Computer outputs a goodbye message and exits
+# - ability to add more answers:
+#   - via easter egg question ("add_answers")
+#   - do not let them add the same answer if the eight ball already has that answer
+# - ability to reset answers back to the original bank (hint: think arr.clone)
+#   - via easter egg question ("reset_answers")
+# - ability to have eight ball print all answers
+#   - via easter egg question ("print_answers")
+
+# ==========================================================================================
+
+puts "Ask the Magic Eight Ball a question and type 1 you must, hmmm."
+puts "If done you are, type QUIT you must."
+
+puts "You must unlearn what you have learned."
+puts "Do pigs fly?"
+puts "When the fat lady sings."
+puts "Yes. Si. Oui!"
+puts "No. Non. Nine!"
+puts "Patience you must have, my young Padawan."
+puts "Powerful you have become, the dark side I sense in you."
+puts "Outlook not so good."
+puts "Fear is the path to the Dark Side."
+puts "Many of the truths that we cling to depend on what we do."
